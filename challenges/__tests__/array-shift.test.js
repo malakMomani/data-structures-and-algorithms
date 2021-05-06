1084fd8e4a5c6b572e9faf0c039df321d4b53384
Malak Al-Momani <malak.momani.96@gmail.com>
2021-05-07 00:33:55 +0300

    edit array-shift test

--- challenges/__tests__/array-shift.test.js
+++ challenges/__tests__/array-shift.test.js
@@ -11,8 +11,6 @@ describe('Testing challenge02: Array Shift' ,()=>{
   });
 
   it('FAIL if not inserted in the middle', ()=>{
-    expect(insertShiftArray(numbers,-1) === [1,2,3,4,5,6,-1]).toBeFalsy();
-    expect(insertShiftArray(numbers,-1) === [-1,2,3,4,5,6,1]).toBeFalsy();
-    expect(insertShiftArray(numbers,-1) === [1,2,3,4,5,-1,6]).toBeFalsy();
+    expect(insertShiftArray(numbers,-1).not.toEqual([1,2,3,-1,4,5,6]));
   });
 });
