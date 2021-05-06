336efd31170e250d8f498ce62a9220af07dff09b
Malak Al-Momani <malak.momani.96@gmail.com>
2021-05-07 00:49:46 +0300

    edit the test unit

--- challenges/__tests__/array-shift.test.js
+++ challenges/__tests__/array-shift.test.js
@@ -8,9 +8,9 @@ describe('Testing challenge02: Array Shift' ,()=>{
   it('insert value at the middle of the array and shift the rest of the elements' ,()=>{
     expect(insertShiftArray(arr,'Holidaaaaaaaay'))
       .toEqual(['Saturday','Sunday','Monday','Holidaaaaaaaay','Tuesday','Wedensday','Thursday']);
-  });
-
-  it('FAIL if not inserted in the middle', ()=>{
-    expect(insertShiftArray(numbers,-1).not.toEqual([1,2,3,-1,4,5,6]));
+    expect(insertShiftArray(numbers,-1))
+      .toEqual([1,2,3,-1,4,5,6]);
+    expect(insertShiftArray(numbers,-1).length)
+      .toStrictEqual(8);
   });
 });
